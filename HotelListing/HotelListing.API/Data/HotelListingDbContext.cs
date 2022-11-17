c601efb639bdb02af8f5329688917f9dfe8b5368
Michael Min <michaelmin2020@gmail.com>
2022-11-17 17:42:22 -0600

    setup seed data for Hotel and Country

--- HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
@@ -34,6 +34,33 @@ namespace HotelListing.API.Data
                             ShortName = "CI"
                         }
                     );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5,
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.7,
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassau",
+                    CountryId = 2,
+                    Rating = 4.3
+                }
+            );
         }
     }
 }
