59da3e5036c4f9a60742b324b9c30f17fe354056
Michael Min <michaelmin2020@gmail.com>
2022-11-17 17:36:03 -0600

    initialized seed data

--- HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
@@ -10,5 +10,30 @@ namespace HotelListing.API.Data
         }
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                        new Country
+                        {
+                            Id = 1,
+                            Name = "Jamaica",
+                            ShortName = "JM"
+                        },
+                        new Country
+                        {
+                            Id = 2,
+                            Name = "Bahamas",
+                            ShortName = "BS"
+                        },
+                        new Country
+                        {
+                            Id = 3,
+                            Name = "Cayman Islands",
+                            ShortName = "CI"
+                        }
+                    );
+        }
     }
 }
