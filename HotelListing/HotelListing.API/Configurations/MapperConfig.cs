9e10bc1007e88ecde0af26e2c200edd7c66802cc
Michael Min <michaelmin2020@gmail.com>
2022-11-26 19:07:23 -0600

    refactored code

--- HotelListing/HotelListing.API/Configurations/MapperConfig.cs
+++ HotelListing/HotelListing.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Country;
+using HotelListing.API.Models.Hotels;
 
 namespace HotelListing.API.Configurations
 {
@@ -9,6 +10,9 @@ namespace HotelListing.API.Configurations
         public MapperConfig()
         {
             CreateMap<Country, CreateCountryDto>().ReverseMap();
+            CreateMap<Country, GetCountryDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Hotel, HotelDto>().ReverseMap();
         }
     }
 }
